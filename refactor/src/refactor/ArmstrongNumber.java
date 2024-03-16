9df3764d836f801fed21dc30b787e4fd51c60bf2
makan <makan@LAPTOP-CFNCCMG5>
2024-03-16 17:47:22 +0530

    commit it

---
+++ refactor/src/refactor/ArmstrongNumber.java
@@ -0,0 +1,20 @@
+package refactor;
+
+public class ArmstrongNumber {
+	public boolean isArmstrong(int number) {
+        int originalNumber = number;
+        int numDigits = String.valueOf(number).length();
+        int sum = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            sum += Math.pow(digit, numDigits);
+            number /= 10;
+        }
+
+        return originalNumber == sum;
+    }
+}
+
+	
+       
