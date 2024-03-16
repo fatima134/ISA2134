c9296a0fe547418dd49d7310de61937b17340a1d
makan <makan@LAPTOP-CFNCCMG5>
2024-03-16 20:38:38 +0530

    commit

---
+++ refactor/src/refactor/Person.java
@@ -0,0 +1,27 @@
+package refactor;
+
+public class Person {
+	
+	    private String name;
+	    private int age;
+
+	    // Constructor
+	    public Person(String name, int age) {
+	        this.name = name;
+	        this.age = age;
+	    }
+
+	    // Getter for name
+	    public String getName() {
+	        return name;
+	    }
+
+	    // Getter for age
+	    public int getAge() {
+	        return age;
+	    }
+	}
+
+
+	
+	   
