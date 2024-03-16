90bb4179d8ffc50c5d6d36c42b6bff3023f31c02
makan <makan@LAPTOP-CFNCCMG5>
2024-03-17 00:36:35 +0530

    commit

---
+++ refactor/src/refactor/Dog.java
@@ -0,0 +1,32 @@
+package refactor;
+
+public class Dog {
+    private String name;
+    private String breed;
+
+    public Dog(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+
+    // Setter methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+}
+
+
+
