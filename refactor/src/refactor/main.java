2837448b7154c84e650e0857b4d20941ac3e5179
makan <makan@LAPTOP-CFNCCMG5>
2024-03-19 17:43:47 +0530

    commit

---
+++ refactor/src/refactor/main.java
@@ -0,0 +1,33 @@
+package refactor;
+
+
+	import java.text.DecimalFormat;
+
+	public class  main  {
+		    public static void main(String[] args) {
+		        Circle circle = new Circle(5);
+	System.out.println("Circle - Area: " + formatDecimal(circle.calculateArea()) + ", Perimeter: " + formatDecimal(circle.calculatePerimeter()));
+
+		
+		        Triangle triangle = new Triangle(3, 4, 5);
+		        System.out.println("Triangle - Area: " + formatDecimal(triangle.calculateArea()) + ", Perimeter: " + formatDecimal(triangle.calculatePerimeter()));
+		    }
+
+		    private static String formatDecimal(double value) {
+		        DecimalFormat df = new DecimalFormat("#.##");
+		        return df.format(value);
+		    }
+		}
+
+
+		
+
+
+
+
+
+
+
+
+
+
