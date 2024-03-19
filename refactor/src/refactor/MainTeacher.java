268a3aa159babd1e57e386bffe3efc6602886f8e
makan <makan@LAPTOP-CFNCCMG5>
2024-03-19 17:39:11 +0530

    commit

---
+++ refactor/src/refactor/MainTeacher.java
@@ -0,0 +1,27 @@
+package refactor;
+
+import java.util.Scanner;
+
+public class MainTeacher {
+		    public static void main(String[] args) {
+		        Scanner scanner = new Scanner(System.in);
+
+		        System.out.print("Enter the type of teacher: ");
+		        String type = scanner.nextLine();
+
+		        System.out.print("Enter the name of teacher: ");
+		        String name = scanner.nextLine();
+
+		        System.out.print("Enter the subject: ");
+		        String subject = scanner.nextLine();
+
+		        TeacherTeaches teacher = new TeacherTeaches(subject, name);
+
+		        teacher.teaches();
+		        teacher.subjects();
+		        
+		        scanner.close();
+		    }
+		}
+
+
