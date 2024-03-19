69e35834b981320c3fce3b85e9231389f0c73afa
makan <makan@LAPTOP-CFNCCMG5>
2024-03-19 17:25:43 +0530

    COMMIT

---
+++ refactor/src/refactor/VehicleRBCA22134.java
@@ -0,0 +1,24 @@
+package refactor;
+
+public class VehicleRBCA22134 {
+    private int horsepower;
+
+    public VehicleRBCA22134 (int horsepower) {
+        this.horsepower = horsepower;
+    }
+
+    public void drive() {
+        System.out.println("Vehicle is being driven");
+    }
+
+    public int getHorsepower() {
+        return horsepower;
+    }
+
+
+}
+
+	
+
+
+	
